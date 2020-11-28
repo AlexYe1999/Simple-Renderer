c3c75dd6f69796d442b41c332b5ca0ce8567da84
Yeahoo <13868303523@163.com>
2020-11-28 21:11:31 +0800

    finish the Line Drawing

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
-#include"include/renderer.h"
+#include"renderer/renderer.h"
 #include<iostream>
 #include<vector>
 using namespace YeahooQAQ;
 
 int main(int argc, char* argv[]){
     string filename = "./Output.jpg";
-    Renderer randerer(720, 480, Scalar(0, 0, 0));
-    
+    Scalar background_color(255, 255, 255);
+    Scalar line_color(0, 0, 255);
+    Renderer randerer(100, 100, background_color);
+    randerer.StartClock();
 
-
-    
+    randerer.DrawLine(Point2i(50, 50), Point2i(90, 90), line_color);
+    randerer.DrawLine(Point2i(30, 20), Point2i(10, 20), line_color);
+    randerer.GetTimeCost();
 
     randerer.SaveImage(filename);        
 
